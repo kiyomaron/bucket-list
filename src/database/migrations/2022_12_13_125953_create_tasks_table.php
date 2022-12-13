efae0341a1e73805e32edd9f9f258e11afd1395d
kiyomaron <kyn.maron947@gmail.com>
2022-12-13 22:11:58 +0900

    Create tasks table

---
+++ src/database/migrations/2022_12_13_125953_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id('task_id');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->integer('user_id');
+            $table->string('title',255);
+            $table->datetime('ideal_goal_on');
+            $table->boolean('is_achieved');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
