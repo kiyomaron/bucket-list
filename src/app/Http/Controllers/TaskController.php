7bc17f7fa2851b93a23e07b37200a05559c3227c
kiyomaron <kyn.maron947@gmail.com>
2022-12-18 13:21:46 +0900

    Implement task index api

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * タスク一覧 
+     * @return \Illuminate\Support\Collection
+     * */
+
+    public function index()
+    {
+        return Task::orderByDesc('task_id')->get();
+    }
 }
